a10b95d7283cb0f1a75a4fddd6f8772ad740181d
Mexidense <mexidense@gmail.com>
2024-02-24 23:17:42 +0100

    feat: send and receive token to & from Spanish Lottery contract

--- contracts/SpanishLotteryContract.sol
+++ contracts/SpanishLotteryContract.sol
@@ -15,7 +15,7 @@ contract SpanishLotteryContract is ERC20, Ownable {
 
     address public lotteryWinner;
 
-    mapping(address => address) public userSmartContract;
+    mapping(address => address) public usersSmartContract;
 
     function tokenPrice(uint256 _numberOfTokens) internal pure returns (uint256) {
         return _numberOfTokens * (1 ether);
@@ -36,8 +36,99 @@ contract SpanishLotteryContract is ERC20, Ownable {
     function mint(uint256 _amount) public onlyOwner {
         _mint(address(this), _amount);
     }
+
+    function registerUser() internal {
+        address smartContractUser = address(
+            new SpanishLotteryTicketNft(
+                msg.sender,
+                address(this),
+                spanishLotteryNftAddress
+            )
+        );
+
+        usersSmartContract[msg.sender] = smartContractUser;
+    }
+
+    function userInfo(address _account) public view returns (address) {
+        return usersSmartContract[_account];
+    }
+
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        if (usersSmartContract[msg.sender] == address(0)) {
+            registerUser();
+        }
+
+        uint256 cost = tokenPrice(_numberOfTokens);
+        require(msg.value >= cost, "Insufficient token paid");
+
+        uint256 totalAllowedTokens = balanceTokensSmartContract();
+        require(
+            _numberOfTokens <= totalAllowedTokens,
+            "Smart contract lottery does not have this number of token to sell"
+        );
+
+        uint256 returnValue = msg.value - cost;
+        payable(msg.sender).transfer(returnValue);
+
+        _transfer(address(this), msg.sender, _numberOfTokens);
+    }
+
+    function backTokens(uint _numberOfTokens) public payable {
+        require(_numberOfTokens > 0, "Required tokens must be greater than zero");
+        require(_numberOfTokens <= balanceTokens(msg.sender), "Required tokens must be less than number of user's tokens");
+
+        _transfer(msg.sender, address(this), _numberOfTokens);
+
+        payable(msg.sender).transfer(tokenPrice(_numberOfTokens));
+    }
 }
 
 contract SpanishLotteryNft is ERC721 {
-    constructor() ERC721("SpanishLotteryNFT", "SLN") {}
+    address public smartContractAddress;
+
+    constructor() ERC721("SpanishLotteryNFT", "SLN") {
+        smartContractAddress = msg.sender;
+    }
+
+    function safeMint(address _owner, uint256 _ticketId) public {
+        require(
+            msg.sender == SpanishLotteryContract(smartContractAddress).userInfo(_owner),
+            "Only owner smart contract can mint a NFT ticket"
+        );
+
+        _safeMint(_owner, _ticketId);
+    }
+}
+
+contract SpanishLotteryTicketNft {    
+    struct Owner {
+        address ownerAddress;
+        address smartContractAddress;
+        address smartContractNftAddress;
+        address smartContractLotteryTicketAddress;
+    }
+
+    Owner public owner;
+
+    constructor(
+        address _userAddress,
+        address _smartContractAddress,
+        address _smartContractNftAddress
+    ) {
+        owner = Owner(
+            _userAddress,
+            _smartContractAddress,
+            _smartContractNftAddress,
+            address(this)
+        );
+    }
+
+    function mintTicket(address _owner, uint _ticketId) public {
+        require(
+            msg.sender == owner.smartContractAddress,
+            "Smart contract lottery cannot create a lottery ticket"
+        );
+
+        SpanishLotteryNft(owner.smartContractNftAddress).safeMint(_owner, _ticketId);
+    }
 }
